3cd2543e8b24a2da621819263bfd93e3e3333dc5
BBaoVanC <bbaovanc@bbaovanc.com>
2025-01-12 00:37:05 -0600

    chore: Add @next/mdx
    https://nextjs.org/docs/app/building-your-application/configuring/mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
+import createMDX from '@next/mdx'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
 }
 
-export default nextConfig
+const withMDX = createMDX({})
+
+export default withMDX(nextConfig)
