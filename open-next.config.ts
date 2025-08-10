db9e6eb8c36cc435698d27a1d9c0b9a22aa157ae
Dominic Frye <me@itsnebula.net>
2025-08-10 12:28:56 -0400

    fix(wrangler): we dont need any of this

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,4 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
 
-export default defineCloudflareConfig({
-	incrementalCache: kvIncrementalCache,
-});
+export default defineCloudflareConfig();
